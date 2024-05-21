d953ca97b81b8bb7850f25c20d803191685b09d3
Andrew <andrew76280@gmail.com>
2024-05-20 21:04:37 -0400

    feat: Reverted back to original version

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -32,7 +32,7 @@ export default function Nav() {
   };
 
   return (
-    <Navbar className="bg-white my-auto">
+    <Navbar onMenuOpenChange={setIsMenuOpen} isMenuOpen={isMenuOpen} className="bg-white my-auto">
       <NavbarContent className="my-10 flex justify-between items-center">
         <NavbarMenuToggle
           aria-label={isMenuOpen ? "Close menu" : "Open menu"}
@@ -47,20 +47,37 @@ export default function Nav() {
         </NavbarItem>
 
         <div className="ml-auto hidden sm:flex gap-4 items-center my-10 justify-center text-blue-700">
-          {menuItems.map((item) => (
-            <NavbarItem key={item.name}>
-              <a
-                className="text-blue-700 no-underline"
-                href={item.link}
-                onClick={(event) => handleClick(event, item.link.slice(1))}
-              >
-                {item.name}
-              </a>
-            </NavbarItem>
-          ))}
+          <NavbarItem>
+            <a
+              className="text-blue-700 no-underline"
+              href="#aboutme"
+              onClick={(event) => handleClick(event, "aboutme")}
+            >
+              ABOUT ME
+            </a>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <a
+              className="text-blue-700 no-underline"
+              href="#projects"
+              onClick={(event) => handleClick(event, "projects")}
+            >
+              PROJECTS
+            </a>
+          </NavbarItem>
+          <NavbarItem isActive>
+            <a
+              className="text-blue-700 no-underline"
+              href="#experience"
+              onClick={(event) => handleClick(event, "experience")}
+            >
+              EXPERIENCE
+            </a>
+          </NavbarItem>
           <NavbarItem>
             <button
               className="bg-blue-700 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+              href="#contact"
               onClick={(event) => handleClick(event, "contact")}
             >
               CONTACT ME
@@ -69,15 +86,15 @@ export default function Nav() {
         </div>
       </NavbarContent>
 
-      <NavbarMenu isOpen={isMenuOpen} className="bg-primary text-foreground items-center">
+      <NavbarMenu className="bg-primary text-foreground items-center" isOpen={isMenuOpen}>
         {menuItems.map((item, index) => (
           <NavbarMenuItem key={`${item.name}-${index}`}>
             <a
               className="text-white no-underline"
               href={item.link}
               onClick={(event) => {
-                setIsMenuOpen(false);
-                
+                event.preventDefault();
+                handleClick(event, item.link.slice(1));
               }}
             >
               {item.name}
