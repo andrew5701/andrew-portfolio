9b55549738c1c8744253ebe826fb0ef42a887e3f
Andrew <andrew76280@gmail.com>
2024-05-23 16:09:15 -0400

    refactor: Swapped the order of the image and text

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -15,13 +15,6 @@ function AboutMe() {
   return (
     <section id="aboutme" className="py-10 px-4 sm:px-6 lg:px-8 bg-white mt-7">
       <div className="flex flex-col lg:flex-row items-center justify-center w-full mt-8 lg:space-x-2 mx-auto">
-        <div className="lg:w-1/2">
-          <h1 className="text-blue-700 text-3xl sm:text-4xl font-light font-extrabold mb-4">ABOUT ME</h1>
-          <p className="mb-5 text-left text-lg text-blue-700 px-5 lg:px-0 max-w-2xl text-base sm:text-lg">
-            A few words that describe me are passionate, hardworking, and dedicated. I am a software engineer with a strong background in Java, Python, and SQL. I have experience in developing web applications using HTML, CSS, and JavaScript. I am also familiar with version control systems like Git. I am excited to continue learning and growing as a software engineer.
-          </p>
-        </div>
-        
         <div className="lg:w-1/2 flex justify-center">
           <img
             className="rounded-md shadow-lg border-gray-200 border-large "
@@ -30,6 +23,12 @@ function AboutMe() {
             width={"300px"}
             height={"300px"}
           />
+        </div>
+        <div className="lg:w-1/2">
+          <h1 className="text-blue-700 text-3xl sm:text-4xl font-light font-extrabold mb-4">ABOUT ME</h1>
+          <p className="mb-5 text-left text-lg text-blue-700 px-5 lg:px-0 max-w-2xl text-base sm:text-lg">
+            A few words that describe me are passionate, hardworking, and dedicated. I am a software engineer with a strong background in Java, Python, and SQL. I have experience in developing web applications using HTML, CSS, and JavaScript. I am also familiar with version control systems like Git. I am excited to continue learning and growing as a software engineer.
+          </p>
         </div>
       </div>
     </section>
