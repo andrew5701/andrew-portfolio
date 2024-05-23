23802079f6a2c3e88d01e73ff7ac2c60fdeb23b4
Andrew <andrew76280@gmail.com>
2024-05-23 11:27:58 -0400

    feat: Updated look of contact me section to include new gif

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -40,61 +40,65 @@ export default function ContactForm() {
 
   return (
     <>
-      <section id="contact" className="mt-30">
-        <h1 className="text-white mt-16 text-5xl"> Contact Me</h1>
-        <p className="text-white text-center text-lg">
-          You can contact me at <a href="mailto:andrew.w.krasuski@gmail.com">andrew.w.krasuski@gmail.com</a>, or here.
-          </p>
-        <form
-          onSubmit={handleSubmit}
-          className="flex flex-col text-white space-y-4"
-        >
-          <div className="w-90 md:w-1/2 mx-auto">
-            <h3 className="text-white text-left">Name</h3>
-            <input
-              type="text"
-              name="name"
-              className="text-black p-2 rounded w-full min-w-5 flex-shrink-0"
-              minLength={10}
-              maxLength={20}
-              required
-            />
-            <h3 className="text-white text-left">Email</h3>
-            <input
-              type="email"
-              name="email"
-              className="text-black p-2 rounded w-full min-w-5"
-              minLength={10}
-              maxLength={50}
-              required
-            />
-            {/* <h3 className="text-white text-left">Subject</h3>
-            <input
-              type="text"
-              name="subject"
-              className="text-black p-2 rounded w-full min-w-5"
-              minLength={10}
-              maxLength={30}
-              required
-            /> */}
-            <h3 className="text-white text-left">Message</h3>
-            <textarea
-              name="message"
-              className="text-black p-2 h-56 rounded w-full min-w-5"
-              minLength={10}
-              maxLength={300}
-              required
-            ></textarea>
-            <button
-              type="submit"
-              className="bg-blue-500 mb-10 mt-10 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full"
-            >
-              <FontAwesomeIcon icon={faPaperPlane} className="mr-2" />
-              Send Message
-            </button>
+      <section id="contact" className="mt-30 bg-custom-gradient py-1 ">
+        <div className="container mx-auto flex flex-col lg:flex-row items-center justify-center mb-9 lg:space-x-8">
+          <div className="lg:w-1/2 flex flex-col items-center lg:items-start text-white">
+            <h1 className="text-white mt-16 mt-5 text-5xl mb-4 font-light">CONTACT ME</h1>
+            <p className="text-center lg:text-left text-lg mb-8">
+              You can contact me at <a href="mailto:andrew.w.krasuski@gmail.com" className="underline text-white">andrew.w.krasuski@gmail.com</a>, or here.
+            </p>
+            <form onSubmit={handleSubmit} className="flex flex-col text-white space-y-4 w-full max-w-lg">
+              <div>
+                <h3 className="text-white text-left">Name</h3>
+                <input
+                  type="text"
+                  name="name"
+                  className="text-black p-2 rounded w-full"
+                  minLength={10}
+                  maxLength={20}
+                  required
+                />
+              </div>
+              <div>
+                <h3 className="text-white text-left">Email</h3>
+                <input
+                  type="email"
+                  name="email"
+                  className="text-black p-2 rounded w-full"
+                  minLength={10}
+                  maxLength={50}
+                  required
+                />
+              </div>
+              <div>
+                <h3 className="text-white text-left">Message</h3>
+                <textarea
+                  name="message"
+                  className="text-black p-2 h-56 rounded w-full"
+                  minLength={10}
+                  maxLength={300}
+                  required
+                ></textarea>
+              </div>
+              <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded w-full flex items-center justify-center"
+              >
+                <FontAwesomeIcon icon={faPaperPlane} className="mr-2" />
+                Send Message
+              </button>
+            </form>
           </div>
-        </form>
-
+          
+          <div className="lg:w-1/2 flex justify-center mt-10 lg:mt-0 ">
+            <img
+              src="/gifs/laptop.gif"
+              alt="Laptop with code on the screen and coffee cup gif"
+              className="rounded-md  border-white"
+            />
+          </div>
+        </div>
+  
         {showStatus && (
           <div
             className={`fixed bottom-4 right-4 p-4 rounded shadow-lg ${
@@ -109,4 +113,5 @@ export default function ContactForm() {
       </section>
     </>
   );
+  
 }
