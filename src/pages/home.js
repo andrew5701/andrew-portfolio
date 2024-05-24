246768ef1adfe9b9f92c586d1031ef5e4cbde4b1
Andrew <andrew76280@gmail.com>
2024-05-23 21:07:58 -0400

    feat: Updated the home section to include the video background

--- src/pages/home.js
+++ src/pages/home.js
@@ -41,13 +41,19 @@ export default function Home() {
 
   return (
     <>
-      <section
-        id="home"
-        className="bg-cover bg-center h-screen flex flex-col items-center justify-center relative"
-        style={{ backgroundImage: 'url("/images/home.jpeg")' }}
-      >
-        <div className="flex justify-center mb-8">
-          <div className="text-left">
+      <section id="home" className="h-screen flex flex-col items-center justify-center relative">
+        <video
+          autoPlay
+          loop
+          muted
+          className="absolute w-full h-full object-cover"
+        >
+          <source src="/videos/home_movie.mov" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+
+        <div className="relative z-10 flex justify-center mb-8">
+          <div className="text-center">
             <h1 className="title-font sm:text-5xl text-1xl mb-4 font-medium text-blue-400">
               HELLO. I'M ANDREW.
             </h1>
@@ -57,72 +63,60 @@ export default function Home() {
           </div>
         </div>
 
-        <div className="flex flex-col items-center">
-          <a
-            className="group no-underline text-white bg-blue-400 hover:bg-blue-900 px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer mb-4"
-            href="/files/Andrew_Krasuski_Resume.pdf"
-            download="Andrew_Krasuski_Resume.pdf"
-          >
-            Download Resume
-            <svg
-              stroke="currentColor"
-              fill="currentColor"
-              strokeWidth="0"
-              viewBox="0 0 20 20"
-              aria-hidden="true"
-              className="opacity-60 group-hover:opacity-100 group-hover:translate-y-1 transition-transform duration-300"
-              height="1em"
-              width="1em"
-              xmlns="http://www.w3.org/2000/svg"
+        <div className="relative z-10 flex flex-col items-center gap-4">
+          <div className="flex gap-4">
+            <motion.a
+              className="no-underline relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              whileHover={{ scale: 1.1 }}
+              href="/files/Andrew_Krasuski_Resume.pdf"
+              download="Andrew_Krasuski_Resume.pdf"
             >
-              <path
-                fillRule="evenodd"
-                d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zm3.293-7.707a1 1 0 011.414 0L9 10.586V3a1 1 0 112 0v7.586l1.293-1.293a1 1 0 111.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </a>
+              DOWNLOAD RESUME
+            </motion.a>
 
-          <div className="flex gap-3 mb-4">
-            <a
-              className="p-4 hover:bg-blue-900 bg-blue-600 text-white flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer"
+            <motion.button
+              className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              whileHover={{ scale: 1.1 }}
+              onClick={(event) => handleClick(event, "projects")}
+            >
+              SEE MY PROJECTS
+              {[...Array(3)].map((_, i) => (
+                <motion.span
+                  key={i}
+                  className="absolute w-full h-full border-2 border-white rounded-full"
+                  initial={{ scale: 0, opacity: 0 }}
+                  whileHover={{
+                    scale: 1.5,
+                    opacity: [1, 0],
+                    transitionEnd: { opacity: 0 },
+                  }}
+                  transition={{ duration: 0.6, delay: i * 0.2, repeat: Infinity }}
+                />
+              ))}
+            </motion.button>
+          </div>
+
+          <div className="flex gap-4">
+            <motion.a
+              className="ml-3 relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md  bg-white hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              whileHover={{ scale: 1.1 }}
               href="https://www.linkedin.com/in/andrew-krasuski/"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaLinkedin className="text-white" size={25} />
-            </a>
+              <FaLinkedin className="text-blue-600" size={25} />
+            </motion.a>
 
-            <a
-              className="p-4 hover:bg-blue-900 bg-blue-600 text-white flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer"
+            <motion.a
+              className="relative inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md bg-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              whileHover={{ scale: 1.1 }}
               href="https://github.com/andrew5701"
               target="_blank"
               rel="noopener noreferrer"
             >
-              <FaGithub className="text-white" size={25} />
-            </a>
+              <FaGithub className="text-black" size={25} />
+            </motion.a>
           </div>
-
-          <motion.button
-            className="relative  inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
-            whileHover={{ scale: 1.1 }}
-            onClick={(event) => handleClick(event, "projects")}
-          >
-            SEE MY PROJECTS
-            {[...Array(3)].map((_, i) => (
-              <motion.span
-                key={i}
-                className="absolute w-full h-full border-2 border-white rounded-full"
-                initial={{ scale: 0, opacity: 0 }}
-                whileHover={{
-                  scale: 1.5,
-                  opacity: [1, 0],
-                  transitionEnd: { opacity: 0 },
-                }}
-                transition={{ duration: 0.6, delay: i * 0.2, repeat: Infinity }}
-              />
-            ))}
-          </motion.button>
         </div>
       </section>
       <Skills />
