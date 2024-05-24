ce788a7ae811f3a79e378a06114f218daeb1400f
Andrew <andrew76280@gmail.com>
2024-05-24 09:39:42 -0400

    feat: Preload gif and updated spacing of skills section

--- src/components/skills.js
+++ src/components/skills.js
@@ -47,9 +47,9 @@ const Skills = () => {
   };
 
   return (
-    <div className="w-full bg-gradient-to-r from-blue-500 to-blue-950 py-6 -mb-36">
+    <div className="w-full bg-gradient-to-r from-blue-500 to-blue-950 py-6 -mb-36 -mt-6">
       <div className="max-w-screen-xl mx-auto px-4 relative">
-      <h1 className="font-light text-4xl text-center mt-20 text-white font-bold mb-10 mt-4">
+      <h1 className="font-light text-4xl text-center mt-20 text-white  -mb-1 font-bold mb-10 mt-4">
         SKILLS
       </h1>
         <Carousel
---
+++ src/pages/_document.js
@@ -0,0 +1,20 @@
+// pages/_document.js
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel="preload" href="/gifs/home.gif" as="image" />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
