dd770fd83dbde363dea08caea7520c52364d572c
Andrew <andrew76280@gmail.com>
2024-05-23 21:06:58 -0400

    feat: Aligned the ABOUT ME text with the paragraph

--- src/pages/aboutme.js
+++ src/pages/aboutme.js
@@ -14,7 +14,7 @@ import photo from "../images/photo.jpg";
 function AboutMe() {
   return (
     <section id="aboutme" className="py-10 px-4 sm:px-6 lg:px-8 bg-white mt-7">
-      <div className="flex flex-col lg:flex-row items-center justify-center w-full mt-8 lg:space-x-2 mx-auto">
+      <div className="flex flex-col lg:flex-row items-center lg:justify-start w-full mt-8 lg:space-x-2 mx-auto">
         <div className="lg:w-1/2 flex justify-center">
           <img
             className="rounded-md shadow-lg border-gray-200 border-large "
@@ -25,7 +25,7 @@ function AboutMe() {
           />
         </div>
         <div className="lg:w-1/2">
-          <h1 className="text-blue-700 text-3xl sm:text-4xl font-light font-extrabold mb-4">ABOUT ME</h1>
+          <h1 className="text-blue-700 text-3xl sm:text-4xl font-light font-extrabold mb-4 ml-12 text-left">ABOUT ME</h1>
           <p className="mb-5 text-left text-lg text-blue-700 px-5 lg:px-0 max-w-2xl text-base sm:text-lg">
             A few words that describe me are passionate, hardworking, and dedicated. I am a software engineer with a strong background in Java, Python, and SQL. I have experience in developing web applications using HTML, CSS, and JavaScript. I am also familiar with version control systems like Git. I am excited to continue learning and growing as a software engineer.
           </p>
