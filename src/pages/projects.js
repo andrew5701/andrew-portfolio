6d089da7630fd96acff007be1bf7eb551cf8352a
Andrew <andrew76280@gmail.com>
2025-03-22 14:13:46 -0400

    Updated height and width to be consistent

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -1,168 +1,111 @@
-import Carousel from "react-bootstrap/Carousel";
-import ExampleCarouselImage from "../images/ExampleCarouselImage.jpg";
 import "../App.css";
 
 function ProjectShowcase() {
+  const projects = [
+    {
+      title: "Rate My Professor CCSU",
+      src: "/images/rmpccsu.png",
+      alt: "Rate My Professor CCSU",
+      description:
+        "A website for rating your favorite Professors from Central Connecticut State University.",
+      github: "https://github.com/mrjasonwalton00/RateMyProfessor",
+      live: "https://www.ratemyprofessorccsu.com/",
+    },
+    {
+      title: "Gen 1 Pokédex",
+      src: "/images/pokedex.png",
+      alt: "Gen 1 Pokédex",
+      description:
+        "View information about your favorite Pokémon from generation 1.",
+      github: "https://github.com/andrew5701/pokedex-gen1-database",
+    },
+    {
+      title: "EZ-Eats",
+      video: "/videos/ezeats.mp4",
+      description: "Find the nearest restaurants in seconds on Android.",
+      github: "https://github.com/username/project3",
+    },
+    {
+      title: "The Two Man Job",
+      src: "/images/two_man_job.png",
+      alt: "The Two Man Job",
+      description:
+        "Illustrates the Shamir's Secret Sharing Algorithm when two keys are brought together, a secret video plays.",
+      github: "https://github.com/giosroom99/Two-Man-Job",
+    },
+    {
+      title: "Snake Game Multiplayer",
+      src: "/images/snake_game.png",
+      alt: "Snake Game Multiplayer",
+      description:
+        "A recreation of the popular Snake game with two player local multiplayer using the ncurses library in C.",
+      github: "https://github.com/andrew5701/snake-game-multiplayer",
+    },
+    {
+      title: "JavaScript Slot Machine",
+      src: "/images/slots.png",
+      alt: "JavaScript Slot Machine",
+      description:
+        "A simple slot machine game created using HTML, CSS, and JavaScript.",
+      github: "https://github.com/andrew5701/web-slot-machine",
+    },
+  ];
+
   return (
     <section
       id="projects"
-      className="px-6 bg-white lg:px-16 py-10 pt-1"
-      style={{ paddingTop: "12rem" }}
+      className="px-6 bg-white lg:px-16 py-10"
+      style={{ paddingTop: "3rem" }}
     >
-      <h1 className="font-light text-4xl text-center mt-20 text-blue-700 font-bold mb-10 mt-4">
+      <h1 className="font-light text-4xl text-center text-blue-700 font-bold mb-10">
         MY PROJECTS
       </h1>
 
-      <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6 ">
-        <div className="relative ">
-          <img
-            src="/images/rmpccsu.png"
-            alt="Rate My Professor CCSU"
-            className="w-full h-auto rounded-t-md"
-          />
-          <div className="bg-blue-800 text-white p-4 w-full rounded-b-md">
-            <h3 className="text-lg font-bold">Rate My Professor CCSU</h3>
-            <p>
-              A website for rating your favorite Professors from Central
-              Connecticut State University.
-            </p>
-            <button
-              onClick={() =>
-                window.open(
-                  "https://github.com/mrjasonwalton00/RateMyProfessor",
-                  "_blank"
-                )
-              }
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
-            >
-              Go to GitHub
-            </button>
-            <button
-              onClick={() =>
-                window.open("https://www.ratemyprofessorccsu.com/", "_blank")
-              }
-              className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2 mt-2"
-            >
-              View Live Demo
-            </button>
-          </div>
-        </div>
-
-        <div className="relative">
-          <img
-            src="/images/pokedex.png"
-            alt="Gen 1 Pokédex"
-            className="w-full h-auto rounded-t-md"
-          />
-          <div className="bg-blue-800  text-white p-4 w-full rounded-b-md">
-            <h3 className="text-lg font-bold">Gen 1 Pokédex</h3>
-            <p>
-              View information about your favorite Pokémon from generation 1.
-            </p>
-            <button
-              onClick={() =>
-                window.open(
-                  "https://github.com/andrew5701/pokedex-gen1-database",
-                  "_blank"
-                )
-              }
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
-            >
-              Go to GitHub
-            </button>
-          </div>
-        </div>
-
-        <div className="relative">
-          <video width="100%" height="auto" className="rounded-t-md" controls>
-            <source src="/videos/ezeats.mp4" type="video/mp4" />
-            Your browser does not support the video tag.
-          </video>
-          {/* <img
-            src="/images/ezeats.png"
-            alt="EZ Eats"
-            
-          /> */}
-          <div className="bg-blue-800 text-white p-4 w-full rounded-b-md">
-            <h3 className="text-lg font-bold">EZ-Eats</h3>
-            <p>Find the nearest restaurants in seconds on Android.</p>
-            <button
-              onClick={() =>
-                window.open("https://github.com/username/project3", "_blank")
-              }
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
-            >
-              Go to GitHub
-            </button>
-          </div>
-        </div>
-
-
-      <div className="relative">
-        <img
-          src="/images/two_man_job.png"
-          alt="The Two Man Job"
-          className="w-full h-auto rounded-t-md"
-        />
-        <div className="bg-blue-800  text-white p-4 w-full rounded-b-md">
-          <h3 className="text-lg font-bold">The Two Man Job</h3>
-          <p>
-            Illustrates the Shamir's Secret Sharing Algorithm when two keys are
-            brought together, a secret video plays.
-          </p>
-          <button
-            onClick={() =>
-              window.open("https://github.com/giosroom99/Two-Man-Job", "_blank")
-            }
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
+      <div className="grid lg:grid-cols-3 md:grid-cols-2 gap-6">
+        {projects.map((project, index) => (
+          <div
+            className="flex flex-col h-full rounded-md shadow-md border overflow-hidden"
+            key={index}
           >
-            Go to GitHub
-          </button>
-        </div>
-      </div>
+            <div className="aspect-[16/9] w-full">
+              {project.video ? (
+                <video className="w-full h-full object-cover" controls>
+                  <source src={project.video} type="video/mp4" />
+                  Your browser does not support the video tag.
+                </video>
+              ) : (
+                <img
+                  src={project.src}
+                  alt={project.alt}
+                  className="w-full h-full object-cover"
+                />
+              )}
+            </div>
 
-      <div className="relative">
-        <img
-          src="/images/snake_game.png"
-          alt="Snake Game Multiplayer"
-          className="w-full h-auto rounded-t-md"
-        />
-        <div className="bg-blue-800  text-white p-4 w-full rounded-b-md">
-          <h3 className="text-lg font-bold">Snake Game Multiplayer</h3>
-          <p>
-            A recreation of the popular Snake game with two player local multiplayer using the ncurses library in C. 
-          </p>
-          <button
-            onClick={() =>
-              window.open("https://github.com/andrew5701/snake-game-multiplayer", "_blank")
-            }
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
-          >
-            Go to GitHub
-          </button>
-        </div>
-      </div>
-      <div className="relative">
-        <img
-          src="/images/slots.png"
-          alt="JavaScript Slot Machine"
-          className="w-full h-auto rounded-t-md border-3 border-gray-250"
-        />
-        <div className="bg-blue-800  text-white p-4 w-full rounded-b-md">
-          <h3 className="text-lg font-bold">JavaScript Slot Machine</h3>
-          <p>
-            A simple slot machine game created using HTML, CSS, and JavaScript. 
-          </p>
-          <button
-            onClick={() =>
-              window.open("https://github.com/andrew5701/web-slot-machine", "_blank")
-            }
-            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mt-2"
-          >
-            Go to GitHub
-          </button>
-        </div>
-      </div>
+            <div className="flex flex-col justify-between bg-blue-800 text-white p-4 flex-grow">
+              <div>
+                <h3 className="text-lg font-bold">{project.title}</h3>
+                <p className="text-sm">{project.description}</p>
+              </div>
+              <div className="mt-4">
+                <button
+                  onClick={() => window.open(project.github, "_blank")}
+                  className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                >
+                  Go to GitHub
+                </button>
+                {project.live && (
+                  <button
+                    onClick={() => window.open(project.live, "_blank")}
+                    className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded ml-2 mt-2 sm:mt-0"
+                  >
+                    View Live Demo
+                  </button>
+                )}
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </section>
   );
