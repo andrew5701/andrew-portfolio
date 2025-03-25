f1eb6e8dbe4f3610a1b44a5f445654a94db0d953
Andrew <andrew76280@gmail.com>
2025-03-25 11:19:32 -0400

    Changed GitHub text

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -92,7 +92,7 @@ function ProjectShowcase() {
                   onClick={() => window.open(project.github, "_blank")}
                   className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                 >
-                  Go to GitHub
+                  View On GitHub
                 </button>
                 {project.live && (
                   <button
