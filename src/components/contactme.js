3ad24f17a5ab5c11f289964e4a563d5fda39c5fb
Andrew <andrew76280@gmail.com>
2025-03-27 08:00:54 -0400

    Updated laptop gif to take up less space and have a white circle background

--- src/components/contactme.js
+++ src/components/contactme.js
@@ -104,13 +104,16 @@ export default function ContactForm() {
               </button>
             </form>
           </div>
-          <div className="lg:w-1/2 flex justify-center items-center mt-6 lg:mt-0 bg-white rounded-xl shadow-md p-4">
+          <div className="w-full max-w-sm aspect-square flex justify-center items-center mt-6 lg:mt-0 bg-white rounded-full shadow-md p-4">
   <img
     src="/gifs/laptop.gif"
     alt="Laptop with code on the screen and coffee cup gif"
-    className="rounded-lg border border-gray-200 shadow-sm"
+    className="max-w-full max-h-full"
   />
 </div>
+
+
+
         </div>
 
         {showStatus && (
